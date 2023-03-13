695a7031a22d48012c1418f2b9d4c0119a1c2524
Lint Action <lint-action@samuelmeuli.com>
2023-03-13 16:30:23 +0000

    Fix code style issues with Prettier

--- src/const.jsx
+++ src/const.jsx
@@ -5,7 +5,7 @@ export const MODE_LIVESTREAM = 3
 export const DEFAULT_SHADOW =
   "0 100px 80px rgb(0 0 0 / 7%), 0 41.7776px 33.4221px rgb(0 0 0 / 5%), 0 22.3363px 17.869px rgb(0 0 0 / 4%), 0 12.5216px 10.0172px rgb(0 0 0 / 4%), 0 6.6501px 5.32008px rgb(0 0 0 / 3%), 0 2.76726px 2.21381px rgb(0 0 0 / 2%)"
 
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.REACT_APP_API_URL
 export const GITHUB_URL = "https://github.com/k27dong/fydp"
 
 export const EMOTION_MAP = [
